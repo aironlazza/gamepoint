8bb92a0355b32d670bd6994f2488ace07d66b482
aironlazza <137323731+aironlazza@users.noreply.github.com>
2023-08-21 23:19:18 -0300

    search em todas pag

--- script.js
+++ script.js
@@ -20,4 +20,32 @@ function buscarJogos() {
       }
   }
 }
+function buscarAmigos() {
+  let input = document.getElementById('search-social').value
+  input=input.toLowerCase();
+  let x = document.getElementsByClassName('amigos');
+    
+  for (i = 0; i < x.length; i++) { 
+      if (!x[i].innerHTML.toLowerCase().includes(input)) {
+          x[i].style.display="none";
+      }
+      else {
+          x[i].style.display="block";                 
+      }
+  }
+}
+function buscarConfig() {
+  let input = document.getElementById('search-config').value
+  input=input.toLowerCase();
+  let x = document.getElementsByClassName('esq');
+    
+  for (i = 0; i < x.length; i++) { 
+      if (!x[i].innerHTML.toLowerCase().includes(input)) {
+          x[i].style.display="none";
+      }
+      else {
+          x[i].style.display="block";                 
+      }
+  }
+}
 
